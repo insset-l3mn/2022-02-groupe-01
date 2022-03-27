afd745f75f6e9b7096b7ce1a03fea06ae35f220f
Mouad <mouad10wolf@gmail.com>
2022-03-27 19:26:36 +0200

    Frontend: Fix qustions options

--- front/src/Components/Student/Questions.js
+++ front/src/Components/Student/Questions.js
@@ -9,125 +9,137 @@ function Questions() {
 
 
 
-    const [questions, setQuestions] = useState([]);
-    const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [questions, setQuestions] = useState([]);
+	const [currentQuestion, setCurrentQuestion] = useState(0);
 
 
-    let { domainId } = useParams();
+	let { domainId } = useParams();
 
 
-    const onSubmit = data => console.log(data);
+	const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => {
+		let obj = data.question_option.reduce((a, v, key) => ({ ...a, [key]: v}), {}) 
 
 
-    useEffect(() => {
-        const fetchData = async () => {
-          const result = await axios(
-            process.env.REACT_APP_BACKEND_ENDPOINT+'questions/domain/'+domainId,
-          );
-    
-          setQuestions(result.data);
-          
-
-        };
-    
-        fetchData();
-      }, []);
+	};
 
 
+	useEffect(() => {
+		const fetchData = async () => {
+		  const result = await axios(
+			process.env.REACT_APP_BACKEND_ENDPOINT+'questions/domain/'+domainId,
+		  );
+	
+		  setQuestions(result.data);
+		  
 
-    function nextButton(event) {
-        event.preventDefault();
-        // Correct
-
-        console.log(currentQuestion);
-        setCurrentQuestion(currentQuestion+1)
-
-    }
-
-    function prevButton(event) {
-        event.preventDefault();
-        // Correct
-
-        setCurrentQuestion(currentQuestion-1)
-
-    }
-
-    function handleSubmit() {
-
-    }
-    
-    function paginate(event) {
-        event.preventDefault();
+		};
+	
+		fetchData();
+	  }, []);
 
 
-        let index = event.target.getAttribute("data-index");
+
+	function nextButton(event) {
+		event.preventDefault();
+
+		if (currentQuestion < questions.length-1) {
+			setCurrentQuestion(currentQuestion+1)
+		}
+
+	}
+
+	function prevButton(event) {
+		event.preventDefault();
+
+		if (currentQuestion > 0) {
+			setCurrentQuestion(currentQuestion-1)
+		}
+		
+
+	}
+
+	
+	function paginate(event) {
+		event.preventDefault();
 
 
-        setCurrentQuestion(Number(index))
+		let index = event.target.getAttribute("data-index");
 
-    }
-    
-    
 
-        return (
-            
-            <div className='questions-panel'>
-                {questions.length > 0 &&
-                <>
-                <div className='progress mb-4 mt-3'>
-                    <div className='progress-bar progress-bar-striped progress-bar-animated' role='progressbar' aria-valuenow='75' aria-valuemin='0' aria-valuemax='100' style={{width: ((100/questions.length)*currentQuestion+1)+`%`}}></div>
-                </div>
+		setCurrentQuestion(Number(index))
 
-                <nav >
-                    <ul className='pagination pagination-sm'>
+	}
 
-                        {questions.map((object, i) => {
-                            return (
-                                <li onClick={paginate}  className={`page-item` + ((currentQuestion == i) ? ` active` : ``) }><a  data-index={i} className='page-link' href='#'>{i+1}</a></li>
-                            );
-                        })}
+	
 
-                    </ul>
-                </nav>
-                <h4>Question {Number(currentQuestion)+1} : {questions[currentQuestion].question}</h4>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                {
-                    questions.map((object, questionIndex) => {
+		return (
+			
+			<div className='questions-panel'>
+				{questions.length > 0 &&
+				<>
+				<div className='progress mb-4 mt-3'>
+					<div className='progress-bar progress-bar-striped progress-bar-animated' role='progressbar' aria-valuenow='75' aria-valuemin='0' aria-valuemax='100' style={{width: ((100/questions.length)*(currentQuestion+1))+`%`}}></div>
+				</div>
 
-                        return (
-                            <div className={`question ` + ((currentQuestion == questionIndex) ? `show` : `d-none`) }>
-                                {
-                                    JSON.parse(object.options).map((option, i) => {
-                                        return (
-                                            <label className='shadow-sm col-8 card p-3 mb-2' style={{cursor: 'pointer'}} >
-                                                <div className='form-check'>
-                                                    <input className='form-check-input' type='radio' value={option.key} name={`question_option[` + questionIndex +`]` } />
-                                                    <span className='form-check-label'>
-                                                        {option.value}
-                                                    </span>
-                                                </div>
-                                            </label>
-                                        );
-                                    })
-                                }
-                            </div>
-                        );
-                    })
-                }
-                </form>
-                </>
+				<nav >
+					<ul className='pagination pagination-sm'>
 
-                }
-                
-                <div className='my-5'>
-                    <button className='btn btn-secondary' onClick={prevButton}>Precedent</button>
+						{questions.map((object, i) => {
+							return (
+								<li onClick={paginate}  className={`page-item` + ((currentQuestion == i) ? ` active` : ``) }><a  data-index={i} className='page-link' href='#'>{i+1}</a></li>
+							);
+						})}
 
-                    <button className='btn btn-primary float-end' onClick={nextButton}>Suivant</button>
-                </div>
-            </div>
-            
-        );
-        
+					</ul>
+				</nav>
+				<h4 className='my-3'>Question {Number(currentQuestion)+1} : {questions[currentQuestion].question}</h4>
+				<form onSubmit={handleSubmit(onSubmit)}>
+				{
+					questions.map((object, questionIndex) => {
+
+						return (
+							<div className={`question ` + ((currentQuestion == questionIndex) ? `show` : `d-none`) }>
+								{
+									JSON.parse(object.options).map((option, i) => {
+										return (
+											<label className='shadow-sm col-8 card p-3 mb-2' style={{cursor: 'pointer'}} >
+												<div className='form-check'>
+													<input className='form-check-input' {...register(`question_option[` + object.id + `]`)} type='radio' value={option.key} />
+													<span className='form-check-label'>
+														{option.value}
+													</span>
+												</div>
+											</label>
+										);
+									})
+								}
+							</div>
+						);
+					})
+				}
+				<div className='my-5'>
+					<button className='btn btn-secondary' disabled={currentQuestion === 0 ? `true` : ``} onClick={prevButton}>Precedent</button>
+
+					<button className={`btn btn-primary float-end ` + (currentQuestion === questions.length-1 ? `d-none` : ``) } onClick={nextButton}>Suivant</button>
+					<button type="submit" className={`btn btn-success float-end ` + (currentQuestion === questions.length-1 ? `d-block` : `d-none`) } >Terminer</button>
+
+					
+				</div>
+				</form>
+				</>
+
+				}
+				
+				
+
+				<div className='my-5'>
+
+				</div>
+			</div>
+			
+		);
+		
 }
 
 export default Questions;
