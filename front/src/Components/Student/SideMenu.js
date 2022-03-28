73086ab6d3d34ee627f3f20b241bf074713ceac8
Mouad <mouad10wolf@gmail.com>
2022-03-28 02:45:48 +0200

    frontend: fix login style

--- front/src/Components/Student/SideMenu.js
+++ front/src/Components/Student/SideMenu.js
@@ -7,27 +7,7 @@ class SideMenu extends React.Component {
     render() {
         return (
 
-        <div class="side-menu">
-            <div class="p-3">
-
-                <ul class="nav nav-pills flex-column mb-auto">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link active" aria-current="page">
-                            Home
-                        </a>
-                    </li>
-                    
-                    <hr />
-                    <li>
-                        <a href="#" className='nav-link link-dark'>Se Déconnecter</a>
-                    </li>
-                </ul>
-                
-
-                
-            </div>
-        </div>
-
+            <></>
 
             );
     }
