cfd0c4c5f1fd0a0107b2caff2304973c62a2af6e
EL JABRI <90796656+ahmed112s@users.noreply.github.com>
2022-04-04 01:30:43 +0200

    Classe d'entité de Formation à partir de la base de données.

---
+++ back/src/main/java/com/mouad/jakarta/entities/Formation.java
@@ -0,0 +1,126 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mouad.jakarta.entities;
+
+import com.mouad.jakarta.entities.Domains;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+/**
+ *
+ * @author Ahmed
+ */
+@Entity
+@Table(name = "Formation")
+@NamedQueries({
+    @NamedQuery(name = "Formation.findAll", query = "SELECT c FROM Formation c"),
+    @NamedQuery(name = "Formation.findById", query = "SELECT c FROM Formation c WHERE c.id = :id"),
+    @NamedQuery(name = "Formation.findByTitle", query = "SELECT c FROM Formation c WHERE c.title = :title")})
+public class Formation implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 200)
+    @Column(name = "title")
+    private String title;
+    @Basic(optional = false)
+    @NotNull
+    @Lob
+    @Size(min = 1, max = 65535)
+    @Column(name = "content")
+    private String content;
+    @JoinColumn(name = "domain", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Domains domain;
+
+    public Formation() {
+    }
+
+    public Formation(Integer id) {
+        this.id = id;
+    }
+
+    public Formation(Integer id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Domains getDomain() {
+        return domain;
+    }
+
+    public void setDomain(Domains domain) {
+        this.domain = domain;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Formation)) {
+            return false;
+        }
+        Formation other = (Formation) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mouad.jakarta.Formation[ id=" + id + " ]";
+    }
+    
+}
